10f6fed851d9eb820f2dcc46e61949aaa1512e6e
Gabbo693 <gabrielesolazzo43@gmail.com>
2025-09-22 17:44:08 +0200

    Visited/Wish section

---
+++ myVolcanoes/panels.js
@@ -0,0 +1,13 @@
+const panels = ["visited", "wish"];
+let currentIndex = 0;
+
+function nextPanel() {
+    // hide current
+    document.getElementById(panels[currentIndex]).style.display = "none";
+
+    // move to next (loop back if at end)
+    currentIndex = (currentIndex + 1) % panels.length;
+
+    // show next
+    document.getElementById(panels[currentIndex]).style.display = "block";
+}
