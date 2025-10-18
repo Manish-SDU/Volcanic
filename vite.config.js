110067e9c120282d6a6ff9294d0ea411cfbcf668
Manish-SDU <maraj24@student.sdu.dk>
2025-10-18 21:52:56 +0200

    Improved File Distribution

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,23 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/styles.css',
+                'resources/css/home/volcano-animation.css',
+                'resources/css/home/volcano-map.css',
+                'resources/js/main.js',
+                'resources/js/bootstrap.js',
+                'resources/js/home/home.js',
+                'resources/js/home/volcano-animation.js',
+                'resources/js/home/volcano-map.js',
+                'resources/js/profile/profile.js',
+                'resources/js/my-volcanoes/panels.js',
+                'resources/js/my-volcanoes/number_increment.js'
+            ],
             refresh: true,
         }),
-        tailwindcss(),
     ],
 });
