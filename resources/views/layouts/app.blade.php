5f05e283713308755d1b8a748ed56b51375b6172
Lara-Ghi <lara.ghiorzo@hotmail.com>
2025-12-20 00:32:26 +0100

    csrf-token reference added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" type="image/svg+xml" href="{{ asset('images/logo/logo.svg') }}">
     <title>Volcanic - @yield('title', 'Home')</title>
     @vite(['resources/css/styles.css', 'resources/js/main.js'])
